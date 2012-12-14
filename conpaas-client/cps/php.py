2c0312d0756903962d9b6880c7e9b61b36fcbcd8
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2012-12-14 16:35:07 +0000

    conpaas-client: support toggling debug info in the php web hosting service

--- conpaas-client/cps/php.py
+++ conpaas-client/cps/php.py
@@ -15,3 +15,39 @@ class Client(WebClient):
 
         else:
             print code_version, 'enabled'
+
+    def toggle_debug(self, service_id):
+        conf = self.callmanager(service_id, 'get_configuration', False, {})
+        debug_mode = conf['phpconf']['display_errors']
+
+        params = { 'phpconf': {} }
+
+        if debug_mode == 'On':
+            params['phpconf']['display_errors'] = 'Off'
+        else:
+            params['phpconf']['display_errors'] = 'On'
+
+        res = self.callmanager(service_id, "update_php_configuration", 
+            True, params)
+
+        if 'error' in res:
+            print res['error']
+        else:
+            conf = self.callmanager(service_id, 'get_configuration', False, {})
+            print 'Debug mode is now', conf['phpconf']['display_errors']
+
+    def usage(self, cmdname):
+        WebClient.usage(self, cmdname)
+        print "    toggle_debug      serviceid           # enable/disable debug mode" 
+
+    def main(self, argv):
+        command = argv[1]
+
+        if command == 'toggle_debug':
+            try:
+                sid = int(argv[2])
+            except (IndexError, ValueError):
+                self.usage(argv[0])
+                sys.exit(0)
+
+            self.toggle_debug(sid)
