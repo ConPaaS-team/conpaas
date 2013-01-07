16cb9f65a42ba8ddf462f30fc644fe4d38a11b20
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-01-07 14:22:56 +0000

    conpaas-client: implement code version selection for the java service as well

--- conpaas-client/cps/java.py
+++ conpaas-client/cps/java.py
@@ -5,4 +5,13 @@ from web import WebClient
 class Client(WebClient):
 
     def enable_code(self, service_id, code_version):
-        print 'java'
+        params = { 'codeVersionId': code_version }
+
+        res = self.callmanager(service_id, "update_java_configuration", 
+            True, params)
+
+        if 'error' in res:
+            print res['error']
+
+        else:
+            print code_version, 'enabled'
