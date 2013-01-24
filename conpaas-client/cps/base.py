c9c33ff1c18efc603b906bb25bc005bf33c8ea48
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-01-24 15:02:41 +0000

    conpaas client: make taskfarm creation workaround explicit

--- conpaas-client/cps/base.py
+++ conpaas-client/cps/base.py
@@ -386,10 +386,10 @@ class BaseClient(object):
                     if service_type not in self.available_services():
                         raise IndexError
 
-                    module = getattr(__import__('cps.' + service_type), service_type)
-                    client = module.Client()
-                    # Create the service and leave (if the client does not have a specific create method, the generic create is taken automagically)
-                    return client.create(service_type)
+                    # taskfarm-specific service creation
+                    if service_type == 'taskfarm':
+                        from cps.taskfarm import Client
+                        return Client().create(service_type)
                 except IndexError:
                     self.usage(argv[0])
                     sys.exit(0)
