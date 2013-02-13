7d3f4f1b01aea8129dd02adce786f265494dcdc8
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-02-13 17:42:40 +0000

    conpaas web hosting agent: refactored to inherit from BaseAgent

--- conpaas-services/src/conpaas/services/webservers/agent/internals.py
+++ conpaas-services/src/conpaas/services/webservers/agent/internals.py
@@ -1,5 +1,5 @@
 '''
-Copyright (c) 2010-2012, Contrail consortium.
+Copyright (c) 2010-2013, Contrail consortium.
 All rights reserved.
 
 Redistribution and use in source and binary forms, 
@@ -14,7 +14,7 @@ that the following conditions are met:
     conditions and the following disclaimer in the
     documentation and/or other materials provided
     with the distribution.
- 3. Neither the name of the <ORGANIZATION> nor the
+ 3. Neither the name of the Contrail consortium nor the
     names of its contributors may be used to endorse
     or promote products derived from this software 
     without specific prior written permission.
@@ -47,49 +47,17 @@ from shutil import rmtree
 from threading import Lock
 import pickle, zipfile, tarfile
 
-from conpaas.core.log import create_logger
+from conpaas.core.agent import BaseAgent, AgentException
 from conpaas.services.webservers.agent import role 
 
 from conpaas.core.https.server import HttpErrorResponse, HttpJsonResponse, FileUploadField
 from conpaas.core.expose import expose
 from conpaas.core import git
 
-E_CONFIG_NOT_EXIST = 0
-E_CONFIG_EXISTS = 1
-E_CONFIG_READ_FAILED = 2
-E_CONFIG_CORRUPT = 3
-E_CONFIG_COMMIT_FAILED = 4
-E_ARGS_INVALID = 5
-E_ARGS_UNEXPECTED = 6
-E_ARGS_MISSING = 7
-E_UNKNOWN = 8
-
-E_STRINGS = [
-  'No configuration exists',
-  'Configuration already exists',
-  'Failed to read configuration state of %s from %s', # 2 params
-  'Configuration is corrupted',
-  'Failed to commit configuration',
-  'Invalid arguments',
-  'Unexpected arguments %s', # 1 param (a list)
-  'Missing argument "%s"', # 1 param
-  'Unknown error',
-]
-
-class AgentException(Exception):
-
-    def __init__(self, code, *args, **kwargs):
-      self.code = code
-      self.args = args
-      if 'detail' in kwargs:
-        self.message = '%s DETAIL:%s' % ( (E_STRINGS[code] % args), str(kwargs['detail']) )
-      else:
-        self.message = E_STRINGS[code] % args
-
-
-class WebServersAgent():
+class WebServersAgent(BaseAgent):
 
     def __init__(self, config_parser):
+      BaseAgent.__init__(self, config_parser)
 
       role.init(config_parser)
 
@@ -111,18 +79,19 @@ class WebServersAgent():
 
       self.WebServer = role.NginxStatic
       self.HttpProxy = role.NginxProxy
-
-      self.logger = create_logger(__name__)
  
     def _get(self, get_params, class_file, pClass):
       if not exists(class_file):
-        return HttpErrorResponse(AgentException(E_CONFIG_NOT_EXIST).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_CONFIG_NOT_EXIST).message)
       try:
         fd = open(class_file, 'r')
         p = pickle.load(fd)
         fd.close()
       except Exception as e:
-        ex = AgentException(E_CONFIG_READ_FAILED, pClass.__name__, class_file, detail=e)
+        ex = AgentException(
+            AgentException.E_CONFIG_READ_FAILED, 
+                pClass.__name__, class_file, detail=e)
         self.logger.exception(ex.message)
         return HttpErrorResponse(ex.message)
       else:
@@ -130,15 +99,16 @@ class WebServersAgent():
 
     def _create(self, post_params, class_file, pClass):
       if exists(class_file):
-        return HttpErrorResponse(AgentException(E_CONFIG_EXISTS).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_CONFIG_EXISTS).message)
       try:
         if type(post_params) != dict: raise TypeError()
         p = pClass(**post_params)
       except (ValueError, TypeError) as e:
-        ex = AgentException(E_ARGS_INVALID, detail=str(e))
+        ex = AgentException(AgentException.E_ARGS_INVALID, detail=str(e))
         return HttpErrorResponse(ex.message)
       except Exception as e:
-        ex = AgentException(E_UNKNOWN, detail=e)
+        ex = AgentException(AgentException.E_UNKNOWN, detail=e)
         self.logger.exception(e)
         return HttpErrorResponse(ex.message)
       else:
@@ -147,7 +117,7 @@ class WebServersAgent():
           pickle.dump(p, fd)
           fd.close()
         except Exception as e:
-          ex = AgentException(E_CONFIG_COMMIT_FAILED, detail=e)
+          ex = AgentException(AgentException.E_CONFIG_COMMIT_FAILED, detail=e)
           self.logger.exception(ex.message)
           return HttpErrorResponse(ex.message)
         else:
@@ -163,11 +133,11 @@ class WebServersAgent():
         p.restart()
       except (ValueError, TypeError) as e:
           self.logger.exception(e)
-          ex = AgentException(E_ARGS_INVALID)
+          ex = AgentException(AgentException.E_ARGS_INVALID)
           return HttpErrorResponse(ex.message)
       except Exception as e:
           self.logger.exception(e)
-          ex = AgentException(E_UNKNOWN, detail=e)
+          ex = AgentException(AgentException.E_UNKNOWN, detail=e)
           return HttpErrorResponse(ex.message)
       else:
           try:
@@ -176,28 +146,30 @@ class WebServersAgent():
               fd.close()
           except Exception as e:
               self.logger.exception(ex.message)
-              ex = AgentException(E_CONFIG_COMMIT_FAILED, detail=e)
+              ex = AgentException(AgentException.E_CONFIG_COMMIT_FAILED, detail=e)
               return HttpErrorResponse(ex.message)
           else:
               return HttpJsonResponse()
 
     def _stop(self, get_params, class_file, pClass):
       if not exists(class_file):
-        return HttpErrorResponse(AgentException(E_CONFIG_NOT_EXIST).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_CONFIG_NOT_EXIST).message)
       try:
         try:
           fd = open(class_file, 'r')
           p = pickle.load(fd)
           fd.close()
         except Exception as e:
-          ex = AgentException(E_CONFIG_READ_FAILED, detail=e)
+          ex = AgentException(
+            AgentException.E_CONFIG_READ_FAILED, detail=e)
           self.logger.exception(ex.message)
           return HttpErrorResponse(ex.message)
         p.stop()
         remove(class_file)
         return HttpJsonResponse()
       except Exception as e:
-        ex = AgentException(E_UNKNOWN, detail=e)
+        ex = AgentException(AgentException.E_UNKNOWN, detail=e)
         self.logger.exception(e)
         return HttpErrorResponse(ex.message)
 
@@ -205,24 +177,25 @@ class WebServersAgent():
       ret = {}
   
       if 'port' not in kwargs:
-        raise AgentException(E_ARGS_MISSING, 'port')
+        raise AgentException(AgentException.E_ARGS_MISSING, 'port')
       if not isinstance(kwargs['port'], int):
-        raise AgentException(E_ARGS_INVALID, detail='Invalid "port" value')
+        raise AgentException(AgentException.E_ARGS_INVALID, detail='Invalid "port" value')
       ret['port'] = int(kwargs.pop('port'))
   
       if 'code_versions' not in kwargs:
-        raise AgentException(E_ARGS_MISSING, 'code_versions')
+        raise AgentException(AgentException.E_ARGS_MISSING, 'code_versions')
       ret['code_versions'] = kwargs.pop('code_versions')
   
       if len(kwargs) != 0:
-        raise AgentException(E_ARGS_UNEXPECTED, kwargs.keys())
+        raise AgentException(AgentException.E_ARGS_UNEXPECTED, kwargs.keys())
       return ret
 
     @expose('GET')
     def getWebServerState(self, kwargs):
       """GET state of WebServer"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with web_lock:
         return _get(kwargs, self.webserver_file, self.WebServer)
 
@@ -250,20 +223,23 @@ class WebServersAgent():
     def stopWebServer(self, kwargs):
       """KILL the WebServer"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with self.web_lock:
         return self._stop(kwargs, self.webserver_file, self.WebServer)
 
     def _httpproxy_get_params(self, kwargs):
       ret = {}
       if 'port' not in kwargs:
-        raise AgentException(E_ARGS_MISSING, 'port')
+        raise AgentException(AgentException.E_ARGS_MISSING, 'port')
       if not isinstance(kwargs['port'], int):
-        raise AgentException(E_ARGS_INVALID, detail='Invalid "port" value')
+        raise AgentException(
+            AgentException.E_ARGS_INVALID, detail='Invalid "port" value')
       ret['port'] = int(kwargs.pop('port'))
   
       if 'code_version' not in kwargs:
-        raise AgentException(E_ARGS_MISSING, 'code_version')
+        raise AgentException(
+            AgentException.E_ARGS_MISSING, 'code_version')
       ret['code_version'] = kwargs.pop('code_version')
   
       if 'web_list' in kwargs:
@@ -271,7 +247,8 @@ class WebServersAgent():
       else:
         web_list = []
       if len(web_list) == 0:
-        raise AgentException(E_ARGS_INVALID, detail='At least one web_list is required')
+        raise AgentException(
+            AgentException.E_ARGS_INVALID, detail='At least one web_list is required')
       ret['web_list'] = web_list
   
       if 'fpm_list' in kwargs:
@@ -289,7 +266,8 @@ class WebServersAgent():
     def getHttpProxyState(self, kwargs):
       """GET state of HttpProxy"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with self.httpproxy_lock:
         return self._get(kwargs, self.httpproxy_file, self.HttpProxy)
 
@@ -320,7 +298,8 @@ class WebServersAgent():
     def stopHttpProxy(self, kwargs):
       """KILL the HttpProxy"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with self.httpproxy_lock:
         return self._stop(kwargs, self.httpproxy_file, self.HttpProxy)
 
@@ -328,28 +307,31 @@ class WebServersAgent():
     def _php_get_params(self, kwargs):
       ret = {}
       if 'port' not in kwargs:
-        raise AgentException(E_ARGS_MISSING, 'port')
+        raise AgentException(AgentException.E_ARGS_MISSING, 'port')
       if not isinstance(kwargs['port'], int):
-        raise AgentException(E_ARGS_INVALID, detail='Invalid "port" value')
+        raise AgentException(
+            AgentException.E_ARGS_INVALID, detail='Invalid "port" value')
       ret['port'] = int(kwargs.pop('port'))
       if 'scalaris' not in kwargs:
-        raise AgentException(E_ARGS_MISSING, 'scalaris')
+        raise AgentException(AgentException.E_ARGS_MISSING, 'scalaris')
       ret['scalaris'] = kwargs.pop('scalaris')
       if 'configuration' not in kwargs:
-        raise AgentException(E_ARGS_MISSING, 'configuration')
+        raise AgentException(AgentException.E_ARGS_MISSING, 'configuration')
       if not isinstance(kwargs['configuration'], dict):
-        raise AgentException(E_ARGS_INVALID, detail='invalid "configuration" object')
+        raise AgentException(
+            AgentException.E_ARGS_INVALID, detail='invalid "configuration" object')
       ret['configuration'] = kwargs.pop('configuration')
   
       if len(kwargs) != 0:
-        raise AgentException(E_ARGS_UNEXPECTED, kwargs.keys())
+        raise AgentException(AgentException.E_ARGS_UNEXPECTED, kwargs.keys())
       return ret
 
     @expose('GET')
     def getPHPState(self, kwargs):
       """GET state of PHPProcessManager"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with self.php_lock:
         return self._get(kwargs, self.php_file, role.PHPProcessManager)
 
@@ -357,7 +339,8 @@ class WebServersAgent():
     def checkAgentState(self, kwargs):
       """Check if agent is running - just return an empty response"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       return HttpJsonResponse()
 
     @expose('POST')
@@ -384,7 +367,8 @@ class WebServersAgent():
     def stopPHP(self, kwargs):
       """KILL the PHPProcessManager"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with self.php_lock:
         return self._stop(kwargs, self.php_file, role.PHPProcessManager)
 
@@ -403,22 +387,27 @@ class WebServersAgent():
     def updatePHPCode(self, kwargs):
 
         if 'filetype' not in kwargs:
-          return HttpErrorResponse(AgentException(E_ARGS_MISSING, 'filetype').message)
+          return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_MISSING, 'filetype').message)
         filetype = kwargs.pop('filetype')
       
         if 'codeVersionId' not in kwargs:
-          return HttpErrorResponse(AgentException(E_ARGS_MISSING, 'codeVersionId').message)
+          return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_MISSING, 'codeVersionId').message)
         codeVersionId = kwargs.pop('codeVersionId')
 
         if 'file' not in kwargs:
-          return HttpErrorResponse(AgentException(E_ARGS_MISSING, 'file').message)
+          return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_MISSING, 'file').message)
         file = kwargs.pop('file')
 
         if filetype != 'git' and not isinstance(file, FileUploadField):
-          return HttpErrorResponse(AgentException(E_ARGS_INVALID, detail='"file" should be a file').message)
+          return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_INVALID, detail='"file" should be a file').message)
 
         if len(kwargs) != 0:
-          return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+          return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
 
         if filetype == 'zip': source = zipfile.ZipFile(file.file, 'r')
         elif filetype == 'tar': source = tarfile.open(fileobj=file.file)
@@ -448,7 +437,8 @@ class WebServersAgent():
     def getTomcatState(self, kwargs):
       """GET state of Tomcat6"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with self.tomcat_lock:
         return self._get(kwargs, self.tomcat_file, role.Tomcat6)
 
@@ -457,11 +447,13 @@ class WebServersAgent():
       """Create Tomcat6"""
       ret = {}
       if 'tomcat_port' not in kwargs:
-        return HttpErrorResponse(AgentException(E_ARGS_MISSING, 'tomcat_port').message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_MISSING, 'tomcat_port').message)
       ret['tomcat_port'] = kwargs.pop('tomcat_port')
   
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
   
       with self.tomcat_lock:
         return self._create(ret, self.tomcat_file, role.Tomcat6)
@@ -470,29 +462,35 @@ class WebServersAgent():
     def stopTomcat(self, kwargs):
       """KILL Tomcat6"""
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
       with self.tomcat_lock:
         return self._stop(kwargs, self.tomcat_file, role.Tomcat6)
 
     @expose('UPLOAD')
     def updateTomcatCode(self, kwargs):
       if 'filetype' not in kwargs:
-        return HttpErrorResponse(AgentException(E_ARGS_MISSING, 'filetype').message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_MISSING, 'filetype').message)
       filetype = kwargs.pop('filetype')
   
       if 'codeVersionId' not in kwargs:
-        return HttpErrorResponse(AgentException(E_ARGS_MISSING, 'codeVersionId').message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_MISSING, 'codeVersionId').message)
       codeVersionId = kwargs.pop('codeVersionId')
 
       if 'file' not in kwargs:
-        return HttpErrorResponse(AgentException(E_ARGS_MISSING, 'file').message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_MISSING, 'file').message)
       file = kwargs.pop('file')
 
       if filetype != 'git' and not isinstance(file, FileUploadField):
-        return HttpErrorResponse(AgentException(E_ARGS_INVALID, detail='"file" should be a file').message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_INVALID, detail='"file" should be a file').message)
 
       if len(kwargs) != 0:
-        return HttpErrorResponse(AgentException(E_ARGS_UNEXPECTED, kwargs.keys()).message)
+        return HttpErrorResponse(AgentException(
+            AgentException.E_ARGS_UNEXPECTED, kwargs.keys()).message)
   
       if filetype == 'zip': source = zipfile.ZipFile(file.file, 'r')
       elif filetype == 'tar': source = tarfile.open(fileobj=file.file)
