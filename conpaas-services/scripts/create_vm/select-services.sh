dbc075873f2b0ae66698ea10774830a734a5a449
bertijff <bertijff@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-02-21 13:50:01 +0000

    Script to pre-select the services that the service image is supposed to support

---
+++ conpaas-services/scripts/create_vm/select-services.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+services="php mysql condor ipop git selenium hadoop scalaris xtreemfs cds"
+SERVICES=`echo -n $services | tr '[a-z]' '[A-Z]'`
+USAGE=`echo $services | sed 's/ / | /g'`
+for uc in `echo $SERVICES`; do eval ${uc}_SERVICE=false ; done
+nocasematch=true
+# No service specified means: all
+[ $# == 0 ] && set all
+for i in $@
+do
+	uc=`echo -n $i | tr '[a-z]' '[A-Z]'`
+	lc=`echo -n $i | tr '[A-Z]' '[a-z]'`
+	case $lc in
+	all) for ii in `echo $SERVICES`; do eval ${ii}_SERVICE=true ; done ;;
+	php|mysql|condor|ipop|git|selenium|hadoop|scalaris|xtreemfs|cds) eval ${uc}_SERVICE=true ;;
+	*)	echo "Unknown service \`$i'" ; echo "Usage: $0 [ $USAGE ]" ; exit 1 ;;
+	esac
+done
+nocasematch=false
+
+for i in $SERVICES
+do
+	name=${i}_SERVICE
+	eval echo $name=\${$name}
+done > services_config.cfg
+cat services_config.cfg
