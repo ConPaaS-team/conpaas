23dd65b292d517348d64632dc6e46eab8bf07ac7
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-05-15 14:13:28 +0000

    conpaas-director: add functional tests for xtreemfs' list_policies and set_policy commands

--- conpaas-director/functional-tests/xtreemfs/run-test.sh
+++ conpaas-director/functional-tests/xtreemfs/run-test.sh
@@ -43,6 +43,60 @@ else
     exit 1
 fi
 
+# List OSD selection policies
+if [ "DEFAULT, FQDN, UUID, DCMAP, VIVALDI" = "`cpsclient.py list_policies $xtreemfs_sid osd_sel`" ]
+then
+    echo "OSD selection policies listing OK" | ts
+else
+    echo "OSD selection policies listing FAILED" | ts
+    exit 1
+fi
+
+# Set OSD selection policy to UUID
+if [ "Policy set." = "`cpsclient.py set_policy $xtreemfs_sid osd_sel testvol UUID | grep Policy`" ]
+then
+    echo "OSD selection policy setting OK" | ts
+else
+    echo "OSD selection policy setting FAILED" | ts
+    exit 1
+fi
+
+# List replica selection policies
+if [ "DEFAULT, FQDN, DCMAP, VIVALDI" = "`cpsclient.py list_policies $xtreemfs_sid replica_sel`" ]
+then
+    echo "Replica selection policies listing OK" | ts
+else
+    echo "Replica selection policies listing FAILED" | ts
+    exit 1
+fi
+
+# Set replica selection policy to VIVALDI
+if [ "Policy set." = "`cpsclient.py set_policy $xtreemfs_sid replica_sel testvol VIVALDI | grep Policy`" ]
+then
+    echo "Replica selection policy setting OK" | ts
+else
+    echo "Replica selection policy setting FAILED" | ts
+    exit 1
+fi
+
+# List replication policies
+if [ "ronly, WaR1, WqRq" = "`cpsclient.py list_policies $xtreemfs_sid replication`" ]
+then
+    echo "Replication policies listing OK" | ts
+else
+    echo "Replication policies listing FAILED" | ts
+    exit 1
+fi
+
+# Set replica selection policy to VIVALDI
+if [ "Policy set." = "`cpsclient.py set_policy $xtreemfs_sid replication testvol WaR1 2 | grep Policy`" ]
+then
+    echo "Replication policy setting OK" | ts
+else
+    echo "Replication policy setting FAILED" | ts
+    exit 1
+fi
+
 # Delete test volume
 cpsclient.py delete_volume $xtreemfs_sid testvol > /dev/null
 wait_for_running $xtreemfs_sid
