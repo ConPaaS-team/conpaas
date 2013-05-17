1f846a09fbc9e14f9dd9ca4e29bcefa4d6176ffd
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-05-17 16:57:15 +0000

    conpaas-client: revert changes introduced in r4765 as they break the functional tests

--- conpaas-client/cps/base.py
+++ conpaas-client/cps/base.py
@@ -160,9 +160,7 @@ class BaseClient(object):
             res = self.callapi("start/" + service_type + '/' + cloud, True, data)
         sid = res['sid']
 
-        if cloud is not None:
-            print "In " + cloud + ": ",
-        print "Creating new " + service_type + " manager (sid=" + str(sid) + ") on " + res['manager'] + "... ",
+        print "Creating new manager on " + res['manager'] + "... ",
         sys.stdout.flush()
 
         self.wait_for_state(sid, initial_state)
