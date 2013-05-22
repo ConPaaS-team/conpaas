36327a595c5105a6271e4257dd9ba29f43057aa8
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-05-22 14:30:52 +0000

    conpaas scalaris client: display service info

--- conpaas-client/cps/scalaris.py
+++ conpaas-client/cps/scalaris.py
@@ -3,7 +3,17 @@ from cps.base import BaseClient
 class Client(BaseClient):
 
     def info(self, service_id):
-        BaseClient.info(self, service_id)
+        service = BaseClient.info(self, service_id)
+        
+        nodes = self.callmanager(service['sid'], "list_nodes", False, {})
+        if 'scalaris' in nodes:
+            for node in nodes['scalaris']:
+                params = { 'serviceNodeId': node }
+                details = self.callmanager(service['sid'], 
+                    "get_node_info", False, params)
+
+                print "management server url:",
+                print "http://%s:8000" % details['serviceNode']['ip']
 
     def usage(self, cmdname):
         BaseClient.usage(self, cmdname)
