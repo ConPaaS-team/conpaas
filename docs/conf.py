9cbe78fee1e73600eadf916092f8bfffe438611f
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-06-03 15:22:16 +0000

    conpaas-docs: bump version number

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ copyright = u'2013, Contrail consortium'
 # built documents.
 #
 # The short X.Y version.
-version = '1.1.0'
+version = '1.2.0'
 # The full version, including alpha/beta/rc tags.
 release = '1.1.0'
 
