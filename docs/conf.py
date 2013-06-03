be2d5d23904845938623eb17dc5c5d510fc8932c
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-06-03 17:30:58 +0000

    conpaas-docs: set html_style in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -124,6 +124,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = '/default.css'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
