c5de4d05306c9fc5ed7d8c288fafcdda1d7e1de0
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-06-14 16:58:36 +0000

    conpaas: release 1.2.0-rc4

--- conpaas-client/setup.py
+++ conpaas-client/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-CPSVERSION = '1.2.0-rc3'
+CPSVERSION = '1.2.0-rc4'
 
 long_description = """
 ConPaaS: an integrated runtime environment for elastic Cloud applications 
