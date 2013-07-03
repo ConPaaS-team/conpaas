44b67bf587e993cc29b1ea5e0cd38b9fd55378bf
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-07-03 07:03:17 +0000

    conpaas: update travis.sh to build and test cpsdirector in a virtualenv

--- travis.sh
+++ travis.sh
@@ -8,15 +8,20 @@ cd $CI_HOME/conpaas-director
 # Create fake files/directories not really needed for unit testing
 touch ConPaaS.tar.gz
 mkdir conpaas
-cp -a ../conpaas-services/{config,scripts} conpaas
+cp -a ../conpaas-services/config conpaas
+cp -a ../conpaas-services/scripts conpaas
+
+mkdir -p cpsdirectorconf/certs
 
 # We cannot use system-wide directories
 sed -i s#/etc/cpsdirector#$PWD/cpsdirectorconf# director.cfg.example
 
+python setup.py install
+
 # Create certificates
 DIRECTOR_TESTING=true python cpsconf.py localhost
 
+# Fake tarball
 touch cpsdirectorconf/ConPaaS.tar.gz
 
-python setup.py install
 coverage run --source=cpsdirector test.py
