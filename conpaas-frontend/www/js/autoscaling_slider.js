00ca05dade0045062b987b33d55f4476847f6702
fernandez <fernandez@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-08-06 16:03:47 +0000

    Solved javascript error when using with Java service

--- conpaas-frontend/www/js/autoscaling_slider.js
+++ conpaas-frontend/www/js/autoscaling_slider.js
@@ -1,5 +1,8 @@
- $(function() {
+/* Copyright (C) 2010-2013 by Contrail Consortium. */
+
+$(function() {
 var select = $( "#strategy" );
+try{
 var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
 min: 1,
 max: 5,
@@ -9,22 +12,12 @@ slide: function( event, ui ) {
 select[ 0 ].selectedIndex = ui.value - 1;
 }
 });
+
 $( "#strategy" ).change(function() {
 slider.slider( "value", this.selectedIndex + 1 );
 });
-});
- $(function() {
-var select = $( "#strategy" );
-var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
-min: 1,
-max: 5,
-range: "min",
-value: select[ 0 ].selectedIndex + 1,
-slide: function( event, ui ) {
-select[ 0 ].selectedIndex = ui.value - 1;
 }
-});
-$( "#strategy" ).change(function() {
-slider.slider( "value", this.selectedIndex + 1 );
-});
+catch(err)
+{}
+
 });
