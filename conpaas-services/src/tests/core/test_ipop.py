1943e37b0ebb409b40da256b863e08851165258e
yradenac <yradenac@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-08-06 14:08:57 +0000

    Unit tests for the BOOTSTRAP_NODES variable.

--- conpaas-services/src/tests/core/test_ipop.py
+++ conpaas-services/src/tests/core/test_ipop.py
@@ -6,6 +6,9 @@ from conpaas.core import ipop
 import os
 import mock
 
+import xml.etree.ElementTree as etree
+
+
 IPOP_CONF_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
         '..', '..', '..')
@@ -75,6 +78,46 @@ class TestIPOP(unittest.TestCase):
         print ipop.configure_ipop(
             os.path.join(IPOP_CONF_DIR, 'config', 'ipop'), 
             'test', '127.0.0.0', '255.0.0.0')
+
+    def _check_bootstrap_nodes(self, dest_file, expected):
+        tree = etree.ElementTree().parse(dest_file)
+        result = tree.findall('RemoteTAs/Transport')
+        transports = [res.text for res in result]
+        for exp in expected:
+            self.assertTrue(exp in transports,
+                            "Cannot find expected bootstrap node %s in %s " % (exp, transports))
+
+    @mock.patch('conpaas.core.ipop.IPOP_CONF_DIR', IPOP_CONF_DIR)
+    def test_bootstrap_nodes_unset(self):
+        self.config_parser.set('agent', 'IPOP_IP_ADDRESS', '127.0.0.1')
+        self.config_parser.set('agent', 'IPOP_BASE_IP', '127.0.0.0')
+        self.config_parser.set('agent', 'IPOP_NETMASK', '255.0.0.0')
+        self.config_parser.set('agent', 'CONPAAS_HOME', IPOP_CONF_DIR)
+        if (self.config_parser.has_option('agent', 'IPOP_BOOTSTRAP_NODES')):
+            self.config_parser.remove_option('agent', 'IPOP_BOOTSTRAP_NODES')
+        ipop.configure_conpaas_node(self.config_parser)
+        dest_file = os.path.join(IPOP_CONF_DIR, 'bootstrap.config')
+
+        # a few bootstrap nodes from default bootstrap.config file
+        expected = ["brunet.tcp://193.136.166.56:49302",
+                    "brunet.udp://193.136.166.56:49302",
+                    "brunet.udp://169.226.40.4:49302",
+                    "brunet.tcp://156.62.231.244:49302",
+                    ]
+        self._check_bootstrap_nodes(dest_file, expected)
+
+    @mock.patch('conpaas.core.ipop.IPOP_CONF_DIR', IPOP_CONF_DIR)
+    def test_bootstrap_nodes_set(self):
+        bootstrap_nodes = 'udp://192.168.122.1:40000\ntcp://192.168.54.3:40001'
+        self.config_parser.set('agent', 'IPOP_IP_ADDRESS', '127.0.0.1')
+        self.config_parser.set('agent', 'IPOP_BASE_IP', '127.0.0.0')
+        self.config_parser.set('agent', 'IPOP_NETMASK', '255.0.0.0')
+        self.config_parser.set('agent', 'CONPAAS_HOME', IPOP_CONF_DIR)
+        self.config_parser.set('agent', 'IPOP_BOOTSTRAP_NODES', bootstrap_nodes)
+        ipop.configure_conpaas_node(self.config_parser)
+        dest_file = os.path.join(IPOP_CONF_DIR, 'bootstrap.config')
+        expected = ["brunet." + node for node in bootstrap_nodes.split()]
+        self._check_bootstrap_nodes(dest_file, expected)
 
 if __name__ == "__main__":
     unittest.main()
