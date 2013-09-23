422bcc264465d2ad466c47fba127b49ec15e22a9
mnoack <mnoack@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-09-23 11:18:54 +0000

    conpaas: xtreemfs: fix for issue CONTRAIL-494

--- conpaas-services/src/conpaas/services/xtreemfs/manager/manager.py
+++ conpaas-services/src/conpaas/services/xtreemfs/manager/manager.py
@@ -599,6 +599,7 @@ class XtreemFSManager(BaseManager):
         volumeName = kwargs.pop('volumeName')
 
         args = [ 'rmfs.xtreemfs',
+                 '-f',
                  '%s:32636/%s' % (self.mrcNodes[0].ip, volumeName) ]
 
         process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
