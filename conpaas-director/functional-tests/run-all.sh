aede31c4ef2736219922c7062a59480a6362507d
ema <ema@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-09-24 08:54:44 +0000

    conpaas functional tests: stop in case of failures

--- conpaas-director/functional-tests/run-all.sh
+++ conpaas-director/functional-tests/run-all.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+
 [ -x "/usr/bin/lynx" ] || exit 1
 
 for service in `find -mindepth 1 -maxdepth 1 -type d | grep -v .svn`
 do
     cd $service 
+    echo "Running test $service"
     ./run-test.sh
     cd ..
 done
