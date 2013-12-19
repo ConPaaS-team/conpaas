6e77c5c07d4d756e4b9f7a9e587fd21dac0e94e5
yradenac <yradenac@ef35cc4a-2640-4bbf-98bc-65920db7d8ae>
2013-12-19 13:46:43 +0000

    Added autoscaling support for the command line client.

--- conpaas-client/cps/php.py
+++ conpaas-client/cps/php.py
@@ -2,6 +2,9 @@ import sys
 
 from web import WebClient
 
+AUTOSCALING_STRATEGIES = ["low", "medium_down", "medium", "medium_up", "high"]
+
+
 class Client(WebClient):
 
     def enable_code(self, service_id, code_version):
@@ -39,17 +42,63 @@ class Client(WebClient):
     def usage(self, cmdname):
         WebClient.usage(self, cmdname)
         print "    toggle_debug      serviceid           # enable/disable debug mode" 
+        print "    on_autoscaling  serviceid  adapt_interval  response_time_objective  strategy   #  enable autoscaling"
+        print "    off_autoscaling  serviceid             #  disable autoscaling"
+
+    def on_autoscaling(self, service_id, cool_down, response_time, strategy):
+        args = {'cool_down': cool_down,
+                'response_time': response_time,
+                'strategy': strategy,
+                }
+        res = self.callmanager(service_id, 'on_autoscaling', True, args)
+        if 'error' in res:
+            print "Error: %s" % res['error']
+
+    def off_autoscaling(self, service_id):
+        res = self.callmanager(service_id, 'off_autoscaling', True, {})
+        if 'error' in res:
+            print "Error: %s" % res['error']
 
     def main(self, argv):
         WebClient.main(self, argv)
 
         command = argv[1]
 
-        if command == 'toggle_debug':
+        if command in ['toggle_debug', 'on_autoscaling', 'off_autoscaling']:
+            # second argument must be the service identifier
             try:
                 sid = int(argv[2])
-            except (IndexError, ValueError):
-                self.usage(argv[0])
-                sys.exit(0)
+            except IndexError:
+                print "Error: missing service identifier to command %s." % command
+                sys.exit(1)
+            except ValueError:
+                print "Error: argument '%s' is not a service identifier." % argv[2]
+                sys.exit(1)
 
+        if command == 'toggle_debug':
             self.toggle_debug(sid)
+
+        elif command == 'on_autoscaling':
+            if len(argv) < 6:
+                print "Error: missing arguments to command %s." % command
+            try:
+                cool_down = int(argv[3])
+            except ValueError:
+                print "Error: argument adapt_interval must be an integer (time in minutes)."
+                sys.exit(1)
+            try:
+                response_time = int(argv[4])
+            except ValueError:
+                print "Error: argument response_time_objective must be an integer (time in milliseconds)."
+                sys.exit(1)
+            strategy = int(argv[5])
+            if strategy not in AUTOSCALING_STRATEGIES:
+                print "Error: argument strategy must be one of %s." % AUTOSCALING_STRATEGIES
+                sys.exit(1)
+            self.on_autoscaling(sid, cool_down, response_time, strategy)
+
+        elif command == 'off_autoscaling':
+            self.off_autoscaling(sid)
+
+        else:
+            print "Error: unknown sub-command '%s'." % command
