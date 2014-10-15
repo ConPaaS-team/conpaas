9bdcefaea259972a3b45e81aa0a9be01b673137e
Guillaume Pierre <guillaume.pierre@irisa.fr>
2014-10-15 16:49:34 +0200

    Frontend: Updated version number information in the service creation page

--- conpaas-frontend/www/create.php
+++ conpaas-frontend/www/create.php
@@ -36,12 +36,12 @@ $page = new CreatePage();
               <tr class="service">
                   <td class="description"> <img src="images/php.png" height="32" /></td>
                   <td class="radio"><input type="radio" name="type" value="php" /> php</td>
-                  <td class="info"> PHP version 5.2 under Nginx </td>
+                  <td class="info"> PHP version 5.3 with Nginx </td>
               </tr>
               <tr class="service">
                   <td class="description"> <img src="images/java.png" height="32" /></td>
                   <td class="radio"><input type="radio" name="type" value="java" /> java</td>
-                  <td class="info"> Java Servlet container using Apache Tomcat 7.2</td>
+                  <td class="info"> Apache Tomcat 6.0 servlet container </td>
               </tr>
           </table>
           <table class="form" cellspacing="0" cellpading="0">
@@ -51,17 +51,17 @@ $page = new CreatePage();
               <tr class="service">
                   <td class="description"> <img src="images/galera.png" height="32" /></td>
                   <td class="radio"><input type="radio" name="type" value="galera" /> MySQL Galera</td>
-                  <td class="info"> Replicated MySQL database </td>
+                  <td class="info"> MySQL-5.5 database with Galera extensions </td>
               </tr>
               <tr class="service">
                   <td class="description"> <img src="images/scalaris.png" height="32" /></td>
                   <td class="radio"><input type="radio" name="type" value="scalaris" /> scalarix</td>
-                  <td class="info"> In-memory key-value store </td>
+                  <td class="info"> Scalaris-0.7 in-memory key-value store </td>
               </tr>
               <tr class="service">
                   <td class="description"> <img src="images/xtreemfs.png" height="32" /></td>
                   <td class="radio"><input type="radio" name="type" value="xtreemfs" /> xtreemfs</td>
-                  <td class="info"> Distributed file system </td>
+                  <td class="info"> XtreemFS-1.5 distributed file system </td>
               </tr>
           </table>
           <table class="form" cellspacing="0" cellpading="0">
