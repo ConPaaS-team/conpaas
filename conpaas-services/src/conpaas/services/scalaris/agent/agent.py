da6fa85bc4617e8dcb81a8aeb67deed1cfcc4694
jensvfischer <jensvfischer@gmail.com>
2015-01-14 15:26:05 +0100

    scalaris: agent: improve placing # cloud groups < 4
    Improve placing of scalris nodes when the number of cloud groups is < 4.

--- conpaas-services/src/conpaas/services/scalaris/agent/agent.py
+++ conpaas-services/src/conpaas/services/scalaris/agent/agent.py
@@ -63,15 +63,10 @@ class ScalarisAgent(BaseAgent):
           mgmt_server = self.mgmt_server
 
       # if the node is the first node in the cloud group it joins at an specific id
-      join_at = ""
-      if kwargs.get('is_firstnode'):
-          key = {0: 3, 1: 7, 2: 'B', 3: 'F'}[kwargs.get('cloud_group')]  # translate cloud_group to key for the id
-          node_id = "16#" + str(key) + "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
-          join_at = ' -k ' + node_id + ' '
-
+      join_at = self._join_at(kwargs.get('cloud_group'), kwargs.get('first_in_group'), kwargs.get('number_of_cloudgroups'))
 
       self.logger.info('writing config')
-      self._write_config(known_hosts, mgmt_server, kwargs.pop('cloud_group'))
+      self._write_config(known_hosts, mgmt_server, kwargs.pop('cloud_group'), kwargs.get('number_of_cloudgroups'))
       dist_erlang_port = ' -e "-kernel inet_dist_listen_min 14194 inet_dist_listen_max 14194"'
       cmd_wo_d = 'sudo -u scalaris /usr/bin/scalarisctl -n node@' + self.ip + ' -p 14195 -y 8000 ' + join_at + flags + dist_erlang_port + ' start'
       self.logger.info('Starting Scalaris node with: %s' % cmd_wo_d)
@@ -90,15 +85,39 @@ class ScalarisAgent(BaseAgent):
       self.logger.info('called shutdownVM %s', res)
       return HttpJsonResponse(res)
 
-    def _write_config(self, known_hosts, mgmt_server, cloud_group):
+    def _join_at(self, cloud_group, first_in_group, number_of_cloudgroups):
+        join_at = ""
+
+        if number_of_cloudgroups > 1 and first_in_group:
+            if number_of_cloudgroups == 2:
+                key = {0: 3, 1: 'B'}[cloud_group]  # translate cloud_group to key for the id
+            elif number_of_cloudgroups == 4:
+                key = {0: 3, 1: 7, 2: 'B', 3: 'F'}[cloud_group]  # translate cloud_group to key for the id
+
+            node_id = "16#" + str(key) + "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+            join_at = ' -k ' + node_id + ' '
+
+        return join_at
+
+    def _write_config(self, known_hosts, mgmt_server, cloud_group, number_of_cloudgroups):
         tmpl = open(self.config_template).read()
         conf_fd = open(self.config_file, 'w')
-        key = {0: 0, 1: 4, 2: 8, 3: 'C'}[cloud_group]  # translate cloud_group to key for the bitmask
+
         template = Template(tmpl, {
                 'known_hosts'      : known_hosts,
                 'mgmt_server'      : mgmt_server,
-                'key'              : key,
                 })
         conf_fd.write(str(template))
+
+        if number_of_cloudgroups > 1:
+            # translate cloud_group to key for the bitmask
+            if number_of_cloudgroups == 2:
+                key = {0: 0, 1: 8}[cloud_group]
+            elif number_of_cloudgroups == 4:
+                key = {0: 0, 1: 4, 2: 8, 3: 'C'}[cloud_group]
+            conf_fd.write("{join_lb_psv, lb_psv_simple}.\n")
+            conf_fd.write("{key_creator, random_with_bit_mask}.\n")
+            conf_fd.write("{key_creator_bitmask, {16#%s0000000000000000000000000000000, 16#3FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF}}.\n" % key)
+            
         conf_fd.close()
         self.logger.info('Scalaris configuration written to %s', self.config_file)
