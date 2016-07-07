ConPaaS-team/conpaas

commit 8007960307788a38e58b6b8f28181418975bce82

Director, manifests: temporarily disable SSL certificate validation

conpaas-director/cpsdirector/manifest.py
@@ -18,10 +18,20 @@ import base64
 import socket
 import urllib2
 import simplejson
+import ssl
 
 from cpsdirector import db
 from cpsdirector.common import log
 from cpsdirector.common import build_response
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 manifest_page = Blueprint('manifest_page', __name__)
 

commit c23b6e37df966a6e31da72ebc34ce10ca97efcbb

cps-tools: proper verification of the director's SSL certificate
Before this commit, all the components (managers, agents, director,
clients) were using the pyOpenSSL library for secure HTTPS
communication, with the exception of the link between the command line
client (cps-tools) and the director, which was implemented using the
standard httplib Python library. While pyOpenSSL was doing proper
verification of the certificates, however, up to Python version 2.7.9,
the standard libraries (httplib) were not doing any HTTPS certificate
verifications. More details here:

https://www.python.org/dev/peps/pep-0476/

Even with Python versions greater than 2.7.9, verification of the
certificates was not done properly in cps-tools because of a bug
introduced in commit 8007960307788a38e58b6b8f28181418975bce82, which
disabled certificate checks globally instead of only when handling
manifests.

Now, this commit changes the way in which cps-tools and the director
communicate. They are now using the same infrastructure as the other
components, using the same helper functions implemented in the ConPaaS
core (which use pyOpenSSL). Server certificates are properly checked
every time and if the certificate verification fails, an error will
be shown to the user. The user is asked for the password only when
installing the certificates (one time), and will be authenticated from
then on only by using the certificate.

The only situation in which HTTPS certificates are not verified is when
downloading resources from external servers during a deployment of an
application from a manifest, as for the ready made applications code has
to be downloaded from https://online.conpaas.eu, which fails
verification. This verification can be easily enabled again by setting
the default value ssl_check=True for the parameter of the read_from_url()
function in cpsdirector/manifest.py (or on a call-by-call basis when this
function is called).

conpaas-director/cpsdirector/cloud.py
@@ -77,7 +77,7 @@ def available_clouds():
         clouds.extend([cloud_name for cloud_name
             in config_parser.get('iaas', 'OTHER_CLOUDS').split(',')
             if config_parser.has_section(cloud_name)])
-    return simplejson.dumps(clouds)
+    return build_response(simplejson.dumps(clouds))
 
 
 def _get_cloud_list(nodes_info):
@@ -163,7 +163,7 @@ def create_nodes():
                     'service_id':service_id, 'service_type':service_type, 'nodes_info':nodes_info,
                     'manager_ip':manager_ip, 'context':context, 'startup_script':startup_script })
 
-    return simplejson.dumps([node.to_dict() for node in nodes])
+    return build_response(simplejson.dumps([node.to_dict() for node in nodes]))
 
 
 
@@ -183,7 +183,7 @@ def remove_nodes():
         res = get_resource_by_id(node['vmid'])
         res.remove()
 
-    return simplejson.dumps({})
+    return build_response(simplejson.dumps({}))
 
 
 
@@ -203,7 +203,7 @@ def create_volume():
 
     log('Created volume %s ' % volume.id)
 
-    return simplejson.dumps({'volume_id':volume.id })
+    return build_response(simplejson.dumps({'volume_id':volume.id }))
 
 
 @cloud_page.route("/attach_volume", methods=['POST'])
@@ -222,7 +222,7 @@ def attach_volume():
 
     log('Attached volume %s to vm %s (cloud: %s)' % (volume_id, vm_id, cloud))
 
-    return simplejson.dumps({})
+    return build_response(simplejson.dumps({}))
 
 
 @cloud_page.route("/detach_volume", methods=['POST'])
@@ -239,7 +239,7 @@ def detach_volume():
 
     log('Detached volume %s (cloud: %s)' % (volume_id, cloud))
 
-    return simplejson.dumps({})
+    return build_response(simplejson.dumps({}))
 
 @cloud_page.route("/destroy_volume", methods=['POST'])
 @cert_required(role='manager')
@@ -255,7 +255,7 @@ def destroy_volume():
 
     log('Destroyed volume %s (cloud: %s)' % (volume_id, cloud))
 
-    return simplejson.dumps({})
+    return build_response(simplejson.dumps({}))
 
 
 # putting this in the user_page wasn't working for some non-obvious reason
@@ -263,7 +263,7 @@ def destroy_volume():
 @cert_required(role='manager')
 def credit():
     log('Manager is asking for credits')
-    return simplejson.dumps({'credit': g.user.credit})
+    return build_response(simplejson.dumps({'credit': g.user.credit}))
 
 
 @cloud_page.route("/list_resources", methods=['GET'])

conpaas-director/cpsdirector/common.py
@@ -62,6 +62,7 @@ def log_error(msg):
 def build_response(data):
     response = make_response(data)
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Connection'] = 'close'
     return response
 
 def error_response(message):

conpaas-director/cpsdirector/manifest.py
@@ -25,15 +25,6 @@ from cpsdirector import db
 from cpsdirector.common import log, log_error, build_response
 from cpsdirector.common import error_response
 
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
-
 manifest_page = Blueprint('manifest_page', __name__)
 
 def check_manifest(json):
@@ -50,17 +41,27 @@ def check_manifest(json):
 
     return True
 
-def read_from_url(url):
-    # reading data may fail for transient reasons, let's try again a few times
+def read_from_url(url, ssl_check=False): # do not check by default
+    if not ssl_check:
+        try:
+            # This will only work in Python versions >= 2.7.9
+            context = ssl._create_unverified_context()
+        except AttributeError:
+            # Legacy Python that doesn't verify HTTPS certificates anyway
+            context = None
+
+    # Reading data may fail for transient reasons, let's try again a few times
     for attempt in range(1, 6):
         try:
-            return urllib2.urlopen(url).read()
+            if context:
+                return urllib2.urlopen(url, context=context).read()
+            else:
+                return urllib2.urlopen(url).read()
         except Exception as ex:
             if attempt == 5:
                 raise ex
             else:
-                time.sleep(2)
-
+                time.sleep(5)
 
 from cpsdirector.application import check_app_exists, get_app_by_id
 from cpsdirector.application import _createapp as createapp
@@ -270,7 +271,7 @@ def download_manifest(appid):
                 appid)
         deleteapp(g.user.uid, appid, True)
 
-    return simplejson.dumps(manifest)
+    return build_response(simplejson.dumps(manifest))
 
 from os.path import exists
 from flask import helpers
@@ -999,7 +1000,6 @@ class MGeneric(MGeneral):
                     return res['error']
 
         return 'ok'
-
 
     #def startup(self, service_id, json, cloud = 'default'):
         #data = {

conpaas-director/cpsdirector/service.py
@@ -154,7 +154,7 @@ def callmanager(app_id, service_id, method, post, data, files=[]):
 @service_page.route("/available_services", methods=['GET'])
 def available_services():
     """GET /available_services"""
-    return simplejson.dumps(valid_services)
+    return build_response(simplejson.dumps(valid_services))
 
 
 from cpsdirector.application import get_default_app, get_app_by_id

conpaas-director/cpsdirector/user.py
@@ -392,8 +392,8 @@ def credit():
     decrement = int(request.values.get('decrement', 1))
     success = _deduct_credit(app_id, decrement)
     if success:
-        return jsonify({'error': False})
-    return jsonify({'error': True})
+        return build_response(jsonify({'error': False}))
+    return build_response(jsonify({'error': True}))
 
 
 @user_page.route("/user_config", methods=['GET'])
@@ -404,8 +404,7 @@ def user_config():
     """
     user = g.user.to_dict()
     user.pop('password', None)
-    res = build_response(simplejson.dumps(user))
-    return res
+    return build_response(simplejson.dumps(user))
 
 
 @user_page.route("/user_credit", methods=['GET'])

conpaas-services/src/conpaas/core/callbacker.py
@@ -88,7 +88,7 @@ class DirectorCallbacker(object):
             director_url = self.config_parser.get('manager', 'DIRECTOR_URL')
             parsed_url = urlparse.urlparse(director_url)
             _, body = https.client.https_post(parsed_url.hostname,
-                                              parsed_url.port or 443,
+                                              parsed_url.port or 5555,
                                               path,
                                               params,
                                               files)

conpaas-services/src/conpaas/core/https/client.py
@@ -72,6 +72,13 @@ def conpaas_init_ssl_ctx(dir, role, uid=None, aid=None):
     __uid = uid
     __aid = aid
 
+def conpaas_init_ssl_ctx_no_certs():
+    global __client_ctx
+    __client_ctx = SSL.Context(SSL.SSLv23_METHOD)
+
+def is_ssl_ctx_initialized():
+    return isinstance(__client_ctx, SSL.Context)
+
 class HTTPSConnection(HTTPConnection):
     """
         This class allows communication via SSL using
@@ -133,7 +140,7 @@ class SSLConnectionWrapper(object):
                 try:
                     buf = self._ssl_conn.recv(_buf_size)
                 except SSL.WantReadError:
-                    time.sleep(1)
+                    # time.sleep(1)
                     continue
                 if not buf:
                     break
@@ -164,7 +171,7 @@ def _conpaas_callback_agent(connection, x509, errnum, errdepth, ok):
         The custom certificate verification function called on the
         agent's client side. The agent might sends requests only to
         other agents pertaining to the same user and the same
-        service.
+        application.
     '''
 
     components = x509.get_subject().get_components()
@@ -185,7 +192,6 @@ def _conpaas_callback_agent(connection, x509, errnum, errdepth, ok):
     if dict['role'] != 'agent':
        return False
 
-    # if dict['UID'] != __uid or dict['serviceLocator'] != __sid:
     if dict['UID'] != __uid or dict['serviceLocator'] != __aid:
        return False
 
@@ -196,7 +202,7 @@ def _conpaas_callback_manager(connection, x509, errnum, errdepth, ok):
     '''
         The custom certificate verification function called on the
         manager's client side. The manager might send requests only to
-        its agents or the frontend.
+        its agents or the director.
 
 	Note: Because of the GIT hook, the manager can be
 	a client to itself (uses its own certificate to connect
@@ -217,6 +223,7 @@ def _conpaas_callback_manager(connection, x509, errnum, errdepth, ok):
             if value == 'CA':
                 return ok
 
+    # FIXME(teodor): the director uses a certificate with the 'frontend' role
     if dict['role'] == 'frontend':
         return ok
 
@@ -224,7 +231,6 @@ def _conpaas_callback_manager(connection, x509, errnum, errdepth, ok):
        return False
 
     if dict['UID'] != __uid or dict['serviceLocator'] != __aid:
-    # if dict['UID'] != __uid or dict['serviceLocator'] != __sid:
        return False
 
     return ok
@@ -233,7 +239,7 @@ def _conpaas_callback_user(connection, x509, errnum, errdepth, ok):
     '''
         The custom certificate verification function called on the
         user's client side. The user might sends requests only to
-        its managers.
+        the director or one of its managers.
     '''
 
     components = x509.get_subject().get_components()
@@ -251,6 +257,10 @@ def _conpaas_callback_user(connection, x509, errnum, errdepth, ok):
             if value == 'CA':
                 return ok
 
+    # FIXME(teodor): the director uses a certificate with the 'frontend' role
+    if dict['role'] == 'frontend':
+        return ok
+
     if dict['role'] != 'manager':
         return False
 
@@ -263,7 +273,7 @@ def _conpaas_callback_director(connection, x509, errnum, errdepth, ok):
     '''
         The custom certificate verification function called on the
         director's client side. The director might sends requests only to
-        managers.
+        managers and agents.
     '''
 
     components = x509.get_subject().get_components()
@@ -275,7 +285,6 @@ def _conpaas_callback_director(connection, x509, errnum, errdepth, ok):
                 return ok
 
     if dict['role'] != 'manager' and dict['role'] != 'agent':
-    # if dict['role'] != 'manager':
        return False
 
     return ok
@@ -303,6 +312,7 @@ def https_get(host, port, uri, params=None):
         h.putrequest('GET', '%s?%s' % (uri, urlencode(params)))
     else:
         h.putrequest('GET', uri)
+    h.putheader('Connection', 'close')
     h.endheaders()
     r = h.getresponse()
     body = r.read()
@@ -328,8 +338,9 @@ def https_post(host, port, uri, params={}, files=[]):
     content_type, body = _encode_multipart_formdata(params, files)
     h = HTTPSConnection(host, port=port, ssl_context=__client_ctx)
     h.putrequest('POST', uri)
-    h.putheader('content-type', content_type)
-    h.putheader('content-length', str(len(body)))
+    h.putheader('Content-Type', content_type)
+    h.putheader('Content-Length', str(len(body)))
+    h.putheader('Connection', 'close')
     h.endheaders()
     h.send(body)
     r = h.getresponse()
@@ -398,13 +409,12 @@ def jsonrpc_get(host, port, uri, method, service_id=0, params=None):
         all_params['params'] = json.dumps(params)
 
     h.putrequest('GET', '%s?%s' % (uri, urlencode(all_params)))
-    h.putheader('content-type', 'application/json')
+    h.putheader('Content-Type', 'application/json')
+    h.putheader('Connection', 'close')
     h.endheaders()
     r = h.getresponse()
     body = r.read()
     h.close()
-
-
     return r.status, body
 
 def jsonrpc_post(host, port, uri, method, service_id=0, params={}):
@@ -426,8 +436,9 @@ def jsonrpc_post(host, port, uri, method, service_id=0, params={}):
     # body = json.dumps({'method': method, 'params': params, 'id': '1'})
     h = HTTPSConnection(host, port=port, ssl_context=__client_ctx)
     h.putrequest('POST', uri)
-    h.putheader('content-type', 'application/json')
-    h.putheader('content-length', str(len(body)))
+    h.putheader('Content-Type', 'application/json')
+    h.putheader('Content-Length', str(len(body)))
+    h.putheader('Connection', 'close')
     h.endheaders()
     h.send(body)
     r = h.getresponse()

cps-tools/src/cps_tools/base.py
@@ -1,13 +1,12 @@
 import getpass
-import httplib
 import os
 import simplejson
 import socket
 import sys
 import time
-import urllib
 import urllib2
-import ssl
+import urlparse
+import traceback
 
 from conpaas.core.https import client
 
@@ -34,12 +33,6 @@ class BaseClient(object):
         self.director_url = None
         self.debug = False
         self.services = None
-
-    def error(self, error_msg, error_code=1):
-        if not error_msg.startswith("ERROR"):
-            error_msg = "ERROR: " + error_msg
-        print error_msg
-        sys.exit(error_code)
 
     def set_director_url(self, dir_url):
         if dir_url is None:
@@ -95,11 +88,11 @@ class BaseClient(object):
                 time.sleep(2)
         return res['state']
 
-    def call_director_post(self, method, data=None, user_certs=True):
-        return self.call_director(method, True, data, user_certs)
+    def call_director_post(self, method, data=None, use_certs=True):
+        return self.call_director(method, True, data, use_certs)
 
-    def call_director_get(self, method, data=None, user_certs=True):
-        return self.call_director(method, False, data, user_certs)
+    def call_director_get(self, method, data=None, use_certs=True):
+        return self.call_director(method, False, data, use_certs)
 
     def call_director(self, method, post, data=None, use_certs=True):
         """Call the director API.
@@ -122,61 +115,80 @@ class BaseClient(object):
 
         if data is None:
             data = {}
-
         data['username'] = self.username
 
-        url = "%s/%s" % (self.director_url, method)
-
         if use_certs:
-            opener = urllib2.build_opener(HTTPSClientAuthHandler(
-                os.path.join(self.confdir, 'key.pem'),
-                os.path.join(self.confdir, 'cert.pem')))
+            # If certificates are used, the ssl context should already have
+            # been initialized with the user's certificates in __init__
+            if not client.is_ssl_ctx_initialized():
+                raise Exception("Cannot call the ConPaaS director:"
+                                " user certificates are not present.\n"
+                                "Try to run 'cps-user get_certificate'"
+                                " first.")
+
+            if self.debug:
+                self.logger.debug("User certificates are present and will be used.")
         else:
-            try:
-                ssl_ctx = ssl._create_unverified_context()
-            except AttributeError:
-                # Legacy Python that doesn't verify HTTPS certificates by default
-                opener = urllib2.build_opener(urllib2.HTTPSHandler())
-            else:
-                # For Python versions >= 2.7.9, we need to explicitly disable certificate validation
-                opener = urllib2.build_opener(urllib2.HTTPSHandler(context=ssl_ctx))
+            # If not, we need to send the user's password and initialize the
+            # ssl context with one that does not use certificates
             data['password'] = self._get_password()
+            client.conpaas_init_ssl_ctx_no_certs()
+
+            if self.debug:
+                self.logger.debug("User certificates will NOT be used.")
+
+        url = "%s/%s" % (self.director_url, method)
 
         if self.debug:
             if 'password' in data:
                 data_log = dict(data)
                 # hiding password from log
                 if data_log['password'] == '':
-                    data_log['password'] = '<empty_password>'
+                    data_log['password'] = '<empty>'
                 else:
-                    data_log['password'] = '<hidden_password>'
+                    data_log['password'] = '<hidden>'
             else:
                 data_log = data
-            self.logger.debug("Requesting %s with data %s." % (url, data_log))
+            self.logger.info("Requesting '%s' with data %s." % (url, data_log))
 
-        data = urllib.urlencode(data)
-
+        parsed_url = urlparse.urlparse(self.director_url)
         try:
             if post:
-                res = opener.open(url, data)
+                status, body = client.https_post(parsed_url.hostname,
+                                                 parsed_url.port or 443,
+                                                 url,
+                                                 data)
             else:
-                url += "?" + data
-                res = opener.open(url)
+                status, body = client.https_get(parsed_url.hostname,
+                                                parsed_url.port or 443,
+                                                url,
+                                                data)
         except:
+            if self.debug:
+                traceback.print_exc()
             ex = sys.exc_info()[1]
-            self.error("Cannot contact the director at URL %s: %s"
-                       % (self.director_url, ex))
+            raise Exception("Cannot contact the director at URL '%s': %s"
+                            % (url, ex))
 
-        rawdata = res.read()
+        if status != 200:
+            raise Exception("Call to method '%s' on '%s' failed: HTTP status %s.\n"
+                            "Params = %s" % (method, url, status, data))
 
         try:
-            res = simplejson.loads(rawdata)
-            if type(res) is dict and 'error' in res:
-                raise Exception(res['error'])
-
-            return res
+            res = simplejson.loads(body)
         except simplejson.decoder.JSONDecodeError:
-            return rawdata
+            # Not JSON, simply return what we got
+            if self.debug:
+                self.logger.info("Call succeeded, result is not json.")
+            return body
+
+        if type(res) is dict and 'error' in res:
+            if self.debug:
+                self.logger.info("Call succeeded, result contains an error")
+            raise Exception(res['error'])
+
+        self.logger.info("Call succeeded, result is: %s" % res)
+        return res
 
     def get_services(self, app_id=None, serv_type=None):
         if self.services is None:
@@ -228,38 +240,69 @@ class BaseClient(object):
         application = self.application_dict(app_id)
 
         if application['manager'] is None:
-            self.error('Application %s has not started. Try to start it first.' % app_id)
+            raise Exception('Application %s has not started. Try to start it first.'
+                            % app_id)
 
         if data is None:
             data = {}
         if files is None:
             files = []
 
-        # File upload
-        if files:
-            res = client.https_post(application['manager'], 443, '/', data, files)
-        # POST
-        elif post:
-            res = client.jsonrpc_post(application['manager'], 443, '/', method, service_id, data)
-        # GET
-        else:
-            res = client.jsonrpc_get(application['manager'], 443, '/', method, service_id, data)
+        # Certificates are always used, the ssl context should already have
+        # been initialized with the user's certificates in __init__
+        if not client.is_ssl_ctx_initialized():
+            raise Exception("Cannot call the manager:"
+                            " user certificates are not present.\n"
+                            "Try to run 'cps-user get_certificate'"
+                            " first.")
+        if self.debug:
+            self.logger.debug("User certificates are present and will be used.")
+            url = "https://%s/" % application['manager']
+            self.logger.info("Requesting '%s' with aid %s, sid %s, method '%s', "
+                              "data %s and %s files." %
+                              (url, app_id, service_id, method, data, len(files)))
 
-        if res[0] == 200:
-            try:
-                data = simplejson.loads(res[1])
-            except simplejson.decoder.JSONDecodeError:
-                # Not JSON, simply return what we got
-                return res[1]
+        try:
+            # File upload
+            if files:
+                status, body = client.https_post(application['manager'], 443,
+                                        '/', data, files)
+            # POST
+            elif post:
+                status, body = client.jsonrpc_post(application['manager'], 443,
+                                        '/', method, service_id, data)
+            # GET
+            else:
+                status, body = client.jsonrpc_get(application['manager'], 443,
+                                        '/', method, service_id, data)
+        except:
+            if self.debug:
+                traceback.print_exc()
+            ex = sys.exc_info()[1]
+            raise Exception("Cannot contact the manager at URL '%s': %s"
+                            % (url, ex))
 
-            res = data.get('result', data)
-            if type(res) is dict and 'error' in res:
-                raise Exception(res['error'])
+        if status != 200:
+            raise Exception("Call to method '%s' on '%s' failed: HTTP status %s.\n"
+                            "Params = %s" % (method, application['manager'],
+                            status, data))
 
-            return res
+        try:
+            data = simplejson.loads(body)
+        except simplejson.decoder.JSONDecodeError:
+            # Not JSON, simply return what we got
+            if self.debug:
+                self.logger.info("Call succeeded, result is not json.")
+            return body
 
-        raise Exception("Call to method %s on %s failed: %s.\nParams = %s" % (
-            method, application['manager'], res[1], data))
+        res = data.get('result', data)
+        if type(res) is dict and 'error' in res:
+            if self.debug:
+                self.logger.info("Call succeeded, result contains an error")
+            raise Exception(res['error'])
+
+        self.logger.info("Call succeeded, result is: %s" % res)
+        return res
 
     def call_manager_post(self, app_id, service_id, method, data=None, files=None):
         if files is None:
@@ -301,17 +344,3 @@ class BaseClient(object):
             output += "\n" + " ".join(rowstr).format(**row)
 
         return output
-
-
-class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
-
-    def __init__(self, key, cert):
-        urllib2.HTTPSHandler.__init__(self)
-        self.key = key
-        self.cert = cert
-
-    def https_open(self, req):
-        return self.do_open(self.get_connection, req)
-
-    def get_connection(self, host, timeout=300):
-        return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)

cps-tools/src/cps_tools/config.py
@@ -44,7 +44,6 @@ def config(description, logger):
     read_files = config_parser.read(config_files)
 
     unread_files = [f for f in config_files if f not in read_files]
-    logger.info("Configuration files successfully read are: %s" % read_files)
     if unread_files != []:
         errors = "\n"
         for f in unread_files:
@@ -52,8 +51,9 @@ def config(description, logger):
                 open(f)
             except:
                 ex = sys.exc_info()[1]
-                errors += "%s\n" % ex
+                errors += "%s" % ex
         logger.info("Skipping configuration files that cannot be read: %s" % errors)
+    logger.info("Configuration files successfully read are: %s" % read_files)
 
     cps_tools_section = "cps-tools"
     conf_values = dict()

cps-tools/src/cps_tools/user.py
@@ -185,11 +185,13 @@ class UserCmd:
         subparser.set_defaults(run_cmd=self.get_certificate, parser=subparser)
 
     def get_certificate(self, args):
-        res = self.client.call_director("getcerts", True, {}, use_certs=False)
+        res = self.client.call_director_post("getcerts", {}, use_certs=False)
 
         zipdata = zipfile.ZipFile(StringIO.StringIO(res))
         zipdata.extractall(path=self.client.confdir)
         https.client.conpaas_init_ssl_ctx(self.client.confdir, 'user')
+
+        print "User certificates installed successfully."
 
     # ========== get_config
     def _add_get_config(self):